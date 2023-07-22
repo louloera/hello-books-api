c1b94c27f2b7c31f7d95cd558765476d56724639
louloera <louloera@gmail.com>
2023-07-21 18:05:50 -0700

    Alot of progress

--- app/models/book.py
+++ app/models/book.py
@@ -6,12 +6,18 @@ class Book(db.Model):
     description = db.Column(db.String)
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
     author = db.relationship("Author",back_populates="books")
-    
+    genres = db.relationship("Genre", secondary="book_genre", backref="books")
+
     def to_dict(self):
         book_as_dict={}
         book_as_dict["id"] = self.id
         book_as_dict["title"] = self.title
         book_as_dict["description"] = self.description
+        if self.author:
+            book_as_dict["author"] = self.author.name
+        if self.genres:
+            genre_names = [genre.name for genre in self.genres]
+            book_as_dict["genres"] = genre_names
 
         return book_as_dict
     
